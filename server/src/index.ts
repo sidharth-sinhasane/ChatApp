bc0ed5a5ab187f289df2f37d8fe5150ca51c6e0a
sidharth_sinhasane <sidharth.sinhasane@gmail.com>
2025-01-08 12:27:57 +0530

    init

---
+++ server/src/index.ts
@@ -0,0 +1,42 @@
+import { WebSocketServer,WebSocket } from "ws";
+const wss= new WebSocketServer({port: 3000})
+interface User {
+    socket: WebSocket,
+    room : string
+}
+let socketCollection :User[]= [];
+
+wss.on("connection",(socket)=>{
+    socket.on("message",(message:string)=>{
+        const parsedMessage= JSON.parse(message);
+        if(parsedMessage.type == "join"){
+           socketCollection.push({
+            socket,
+            room:parsedMessage.payload.roomId
+        })
+            
+        }
+        else{
+            let currentRoomId:string= "";
+            //search of roomId
+            for(let i =0 ; i< socketCollection.length;i++){
+                if(socketCollection[i].socket== socket){
+                    currentRoomId=socketCollection[i].room;
+                }
+            }
+            if(currentRoomId == ""){
+                socket.send("not part of any room")
+            }
+
+            for(let i =0 ; i<socketCollection.length;i++){
+                if(socketCollection[i].room== currentRoomId){
+                    socketCollection[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+
+        }
+    })
+    socket.on("disconnect",()=>{
+        socketCollection=socketCollection.filter(x=>x!)
+    })
+})
